9c1526e0df00c4864bbc28a91cfb02434a02c06c
tonny <tny@sawitsejahtera.com>
2017-11-17 16:27:23 +0700

    a

--- application/config/config.php
+++ application/config/config.php
@@ -15,7 +15,9 @@
 |
 */
 //$config['base_url']	= "http://192.168.0.172/neracaNPM/";
-$config['base_url']	= "http://localhost/jetwing/";
+$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
 date_default_timezone_set('Asia/Jakarta');
 
 /*
